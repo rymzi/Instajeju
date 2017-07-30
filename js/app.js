db65be3ff7af457276c419f15766bec0eae2a9ac
rymzi <bapul@hotmail.com>
2017-07-30 16:48:32 +0900

    jquery

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   var $heart = $(".heart");
   $heart.click(function() {
     var hasHeartClass = $(this).hasClass("heart-clicked");
-    if ($(this).hasClass("heart-clicked")) {
+    if (hasHeartClass) {
+      $(this).removeClass("fa-heart");
       $(this).removeClass("heart-clicked");
+      $(this).addClass("fa-heart-0");
     } else {
+      $(this).removeClass("fa-heart-0");
+      $(this).addClass("fa-heart");
       $(this).addClass("heart-clicked");
     }
   });
