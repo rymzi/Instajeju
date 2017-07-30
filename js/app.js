a2f5ebbb8b29189005faccac4d9a881ef95bb710
rymzi <bapul@hotmail.com>
2017-07-30 16:16:52 +0900

    add.js modified heart to jquery.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,6 @@
-var weapons = ["AK47", "Magnum", "Kim jong fat"];
-var soldier = {
-  name: "Nicolas",
-  age: 22,
-  weapon: weapons[2],
-  ksKorean: false,
-  isDead: false
-  sayHi: function() {
-    console.log("Hello, my name is" + this.name + "and I'm:" + this.age);
-  }
-};
-
-function sum(a,b) {
-  return a+b;
-}
-
-function Substrac(a,b) {
-  return a-b;
-}
-
-function Divide(a,b) {
-  return a/b;
-}
-function Expo(a,b) {
-  return a**b;
-}
+$(document).ready(function() {
+  var $heart = $(".heart");
+  $heart.click(function() {
+    console.log("i've been clicked.");
+  });
+});
