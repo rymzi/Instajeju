af47d9ab7de93eb30568b22ae73c1b901b58ec01
rymzi <bapul@hotmail.com>
2017-07-30 16:43:55 +0900

    added heart-clicked animation

---
+++ images/ios_files/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  var $heart = $(".heart");
+  $heart.click(function() {
+    console.log("i've been clicked.");
+  });
+});
--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   var $heart = $(".heart");
   $heart.click(function() {
-    console.log("i've been clicked.");
+    var hasHeartClass = $(this).hasClass("heart-clicked");
+    if ($(this).hasClass("heart-clicked")) {
+      $(this).removeClass("heart-clicked");
+    } else {
+      $(this).addClass("heart-clicked");
+    }
   });
 });
